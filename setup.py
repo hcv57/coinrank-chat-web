7fe5f86dee6286ac35f973c9e804d7d923151ed5
Dukat <dukat@coinrank.chat>
2018-02-23 09:59:22 +0200

    Refactor to use native namespace package

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(
-    name='coinrankchatweb',
-    packages=['coinrankchatweb'],
+    name='coinrankchat-web',
+    packages=['coinrankchat.web'],
     include_package_data=True,
     install_requires=[
         'flask',
